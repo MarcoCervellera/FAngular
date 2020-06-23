d265f3f36a8aa0a715ef8167922bf2a9bfa9a696
Marco Cervellera <ing.marco.cervellera@gmail.com>
2020-06-23 17:56:59 +0200

    initial commit

---
+++ FAngular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {ServicesService} from "./modules/core/services.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  private title:string;
+
+  constructor(servicesService: ServicesService) {
+    this.title = servicesService.demo();
+  }
+}
---
+++ FAngular/src/app/modules/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServicesService } from "./services.service";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ServicesService
+  ]
+})
+export class CoreModule { }
